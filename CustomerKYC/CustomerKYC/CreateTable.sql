c4d3be991ad35a076e9a7845de7d9056d5a1c495
keerthik <keerthik@virtusa.com>
2019-01-21 13:04:42 +0530

    Aadhar Verification

---
+++ CustomerKYC/CustomerKYC/CreateTable.sql
@@ -0,0 +1,3 @@
+use PrimeBankPOCdb;
+create table Adhardetails(AadharNumber bigint, CustomerName varchar(30), Age int, DOB varchar(30), Address varchar(100), MobileNumber bigint, EmailId varchar(50));
+insert into Adhardetails values(987654321012, 'Meghana', 24, '22-05-1996', 'Hyderabad', 9988776655, 'meghana@gmail.com');
